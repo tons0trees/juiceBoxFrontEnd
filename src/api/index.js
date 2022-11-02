ca7587d7c37fb4866716f8633ce33773af886942
tons0trees <forrest@fmcclure.com>
2022-11-02 13:23:24 -0600

    login and getPosts

---
+++ src/api/index.js
@@ -0,0 +1,37 @@
+const url = 'https://secure-gorge-12590.herokuapp.com'
+
+const requestObj = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+}
+
+export async function getPosts(authToken) {
+    if (authToken) {
+        requestObj.headers.Authorization = `Bearer ${authToken}`
+    }
+
+    try {
+        const response = await fetch(`${url}/api/posts`,requestObj)
+
+        const {posts} = await response.json()
+
+        return posts;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export async function login(username, password) {
+    requestObj.method = 'POST'
+    requestObj.body =JSON.stringify({ username, password })
+
+    try {
+        const response = await fetch(`${url}/api/users/login`, requestObj)
+
+        const {token} = await response.json()
+
+        return token
+    } catch (error) {
+        console.error(error)
+    }
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,12 @@
 import React from "react"
 import {Navbar} from "./"
+import { getPosts, login } from "../api"
+
 
 const Main = () => {
+    getPosts()
+    login("albert", "bertie99")
+
     return (
         <div id="main">
             <Navbar />
